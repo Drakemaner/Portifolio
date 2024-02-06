99939b0d44bba6b623993d93ad75bc22e0b668d3
Jenner <guilhermejenner10@gmail.com>
2024-02-06 20:57:55 -0300

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const route : Routes =
+  [
+    {
+      pa
+    }
+   ]
+
+@NgModule({
+  declarations: [],
+
+})
+export class AppRoutingModule { }

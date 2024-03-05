a072a3335f7faf5bd47131037c72a33b3c88f25c
Jenner <guilhermejenner10@gmail.com>
2024-03-05 12:07:00 -0300

    Correção

--- src/app/main/main/main.component.ts
+++ src/app/main/main/main.component.ts
@@ -10,7 +10,7 @@ export class MainComponent implements OnInit {
    eu = {
      nome : 'Guilherme Jenner',
      funcao : 'Desenvolvedor Full Stack',
-     intro : 'Olá, Eu sou um Desenvolvedor Full Stack e este é um site que fiz para ser meu portifólio , sendo desenvolvido em Angular.',
+     intro : 'Olá, Eu sou um Desenvolvedor Full Stack e este é um site que fiz para ser meu portfólio , sendo desenvolvido em Angular.',
      sobreMim:  'Olá! Sou um estudante de Sistemas de Informação na UVV, atualmente no 6° período. ' +
                 'Tenho um forte apreço pela comunicação e sou reconhecido pelo meu esforço e dedicação. ' +
                 'Trabalhar em equipe é uma das minhas habilidades mais destacadas, onde busco sempre contribuir de forma colaborativa e eficaz e ' +
